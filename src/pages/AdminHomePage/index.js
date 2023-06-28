3c864ef4459dca3eea946b2b23f775ddd9a758f9
nguyenhuycuong1 <nhclnhc@gmail.com>
2023-06-28 18:13:15 +0700

    test

--- src/pages/AdminHomePage/index.js
+++ src/pages/AdminHomePage/index.js
@@ -11,7 +11,6 @@ const ORDERMENAGEMENT = 'Order';
 function AdminHomePage() {
     const params = useParams();
     const option = params.option;
-    console.log(option);
     return (
         <div className={cx('wrapper')}>
             {option === USERMENAGEMENT && (
@@ -54,7 +53,71 @@ function AdminHomePage() {
                     </tbody>
                 </table>
             )}
-            {option === PRODUCTMENAGEMENT && <div className={cx('products-wrapper')}></div>}
+            {option === PRODUCTMENAGEMENT && (
+                <div className={cx('products-wrapper')}>
+                    <table className={cx('tablemodule', 'table')}>
+                        <thead class="thead-dark">
+                            <tr>
+                                <th className={cx('field')} scope="col">
+                                    Hình ảnh
+                                </th>
+                                <th className={cx('field')} scope="col">
+                                    Mã sản phẩm
+                                </th>
+                                <th className={cx('field')} scope="col">
+                                    Tên sản phẩm
+                                </th>
+                                <th className={cx('field')} scope="col">
+                                    Mô tả
+                                </th>
+                                <th className={cx('field')} scope="col">
+                                    Giá
+                                </th>
+                                <th className={cx('field')} scope="col">
+                                    Hãng
+                                </th>
+                                <th className={cx('field')} scope="col">
+                                    Thao tác
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <image
+                                        className={cx('product-img', 'img-thumbnail')}
+                                        src="https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/p/r/pro-m2.jpg"
+                                        alt="a234fs345"
+                                    />
+                                </td>
+                                <td>
+                                    <span>a234fs345</span>
+                                </td>
+                                <td>
+                                    <span>MacBook Pro IPS Panel Retina Display 2560x1600 Intel Core i5 2.3GHz 8GB</span>
+                                </td>
+                                <td>
+                                    <span>
+                                        Ultrabook 13.3 IPS Panel Retina Display 2560x1600 128GB SSD Intel Iris Plus
+                                        Graphics 640 macOS 1.37kg
+                                    </span>
+                                </td>
+                                <td>
+                                    <span>34458166.49</span>
+                                </td>
+                                <td>
+                                    <span>Apple</span>
+                                </td>
+                                <td>
+                                    <button className={cx('update-btn')}>Sửa</button>
+
+                                    <button className={cx('delete-btn')}>Xóa</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
     );
 }
